21d024604722a962be314ea450b40fa9a143f2de
Erik Frojdh <erik.frojdh@gmail.com>
2020-05-06 08:41:37 +0200

    non needed constructor

--- src/ImageView.h
+++ src/ImageView.h
@@ -2,10 +2,9 @@
 #include <cstddef>
 #include <cstdint>
 struct ImageView {
-    ImageView() {}
-    ImageView(int64_t frameNumber, char *data)
-        : frameNumber(frameNumber), data(data) {}
+    // ImageView() {}
+    // ImageView(int64_t frameNumber, char *data)
+    //     : frameNumber(frameNumber), data(data) {}
     int64_t frameNumber{-1};
-    size_t size{0};
     char *data{nullptr};
 };
